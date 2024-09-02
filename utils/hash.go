65f76ffbd9dee97b4937cd56b393c5eddcbd1991
Suraj-Encoding <dalvisuraj256@gmail.com>
2024-09-02 20:52:24 +0530

    chore: Update CreateURL function to use environment variable for server URI and include full URL in short_url response

--- utils/hash.go
+++ utils/hash.go
@@ -2,10 +2,13 @@ package utils
 
 import (
 	"log"
+	"os"
+	"strings"
 	"crypto/md5"
 	"encoding/hex"
 	"shortify/db"
 	"shortify/models"
+	"shortify/env"
 )
 
 // # GenerateShortURL : Generate a short URL from the original URL
@@ -18,16 +21,34 @@ func GenerateShortURL(originalURL string) string {
 
 // # CreateURL : Create a new URL in the database
 func CreateURL(originalURL string) string {
+
+	// # Get Short URL
 	shortURL := GenerateShortURL(originalURL)
 	id := shortURL
+
+	// # Load Env Variables
+	env.LoadEnv()
+
+	// # Get Port
+	port := env.GetEnv("PORT", "3000")
+
+	// # Build Short URL with Short URL ID
+	shortURL = os.Getenv("SERVER_URI")
+	shortURL = strings.Replace(shortURL, "{PORT}", port, 1)
+	shortURL = shortURL + "/redirect/" + id
+
+	// # Form URL Model Object
 	url := models.URL{
 		ID:           id,
 		OriginalURL:  originalURL,
 		ShortURL:     shortURL,
 	}
+	// # Save URL
 	err := db.SaveURL(url)
 	if err != nil {
+		// # Saving URL Error
 		log.Fatal("🚫 Saving URL Error: ", err)
 	}
+	// # Return Short URL
 	return shortURL
 }
