9c269b69c59751ec8528cf3a9de8df8f604333d4
Suraj-Encoding <dalvisuraj256@gmail.com>
2024-09-02 18:58:44 +0530

    feat: Add handler for shortening URLs

---
+++ handlers/shortener.go
@@ -0,0 +1,26 @@
+package handlers
+
+import (
+	"encoding/json"
+	"net/http"
+	"shortify/utils"
+)
+
+func ShortURLHandler(w http.ResponseWriter, r *http.Request) {
+	var data struct {
+		URL string `json:"url"`
+	}
+	err := json.NewDecoder(r.Body).Decode(&data)
+	if err != nil {
+		http.Error(w, "Invalid request body", http.StatusBadRequest)
+		return
+	}
+
+	shortURL := utils.CreateURL(data.URL)
+	response := struct {
+		ShortURL string `json:"short_url"`
+	}{ShortURL: shortURL}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(response)
+}
