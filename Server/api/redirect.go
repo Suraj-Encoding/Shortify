b765c836355874dab77771aabf206852dfa41c4d
SdDevX <suraj@leanafy.com>
2025-12-31 14:09:03 +0530

    feat: added redirect logic

--- Server/api/redirect.go
+++ Server/api/redirect.go
@@ -1,13 +1,70 @@
 package api
 
-// # RedirectURLHandler : Redirect to the original URL from the short URL
-// func RedirectURLHandler(w http.ResponseWriter, r *http.Request) {
-// 	id := r.URL.Path[len("/redirect/"):]
-// 	url, err := db.GetURL(id)
-// 	if err != nil {
-// 		// # Invalid Request
-// 		http.Error(w, "🚫 Invalid Request!", http.StatusNotFound)
-// 		return
-// 	}
-// 	http.Redirect(w, r, url.OriginalURL, http.StatusFound)
-// }
+import (
+	"errors"
+	"fmt"
+	"net/http"
+	"shortify/app"
+	"shortify/schema"
+	"shortify/utils"
+	"strings"
+)
+
+// # Redirect 'URL'
+func RedirectURL(w http.ResponseWriter, r *http.Request) {
+	var err error
+	var errMsg string
+	var errRes schema.Error
+
+	// # Get the 'URL Path'
+	urlPath := strings.Trim(r.URL.Path, "/")
+
+	urlPathParts := strings.Split(urlPath, "/")
+	urlPathPartLength := len(urlPathParts)
+
+	if urlPathPartLength != 2 {
+		errMsg = "🚫 Invalid Request To Shortify!"
+		err = errors.New(errMsg)
+		utils.LogError(err, "API.RedirectURL")
+		errRes = schema.Error{
+			StatusCode: http.StatusNotFound,
+			Message:    errMsg,
+		}
+		utils.SetAppError(w, &errRes)
+		return
+	}
+
+	// # Get the 'username' and 'link slug' from the 'URL Path'
+	username := utils.GetTrimmedValue(urlPathParts[0])
+	linkSlug := utils.GetTrimmedValue(urlPathParts[1])
+
+	if username == "" || linkSlug == "" {
+		errMsg = "🚫 Invalid Request To Shortify!"
+		err = errors.New(errMsg)
+		utils.LogError(err, "API.RedirectURL")
+		errRes = schema.Error{
+			StatusCode: http.StatusNotFound,
+			Message:    errMsg,
+		}
+		utils.SetAppError(w, &errRes)
+		return
+	}
+
+	// # Get the 'Destination URL'
+	destinationURL, err := app.GetDestinationURL(username, linkSlug)
+	if err != nil {
+		errMsg = err.Error()
+		errMsg = fmt.Sprintf("🚫 Invalid Request To Shortify: Failed to get the destination URL: %s", errMsg)
+		err = errors.New(errMsg)
+		utils.LogError(err, "API.RedirectURL")
+		errRes = schema.Error{
+			StatusCode: http.StatusInternalServerError,
+			Message:    errMsg,
+		}
+		utils.SetAppError(w, &errRes)
+		return
+	}
+
+	// # Redirect to the 'Destination URL'
+	http.Redirect(w, r, *destinationURL, http.StatusFound)
+}
