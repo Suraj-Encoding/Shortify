9feb598600a84b292abdd2e91b0d5be4c05480cf
Suraj-Encoding <dalvisuraj256@gmail.com>
2026-01-01 15:06:33 +0530

    fronend refactor

--- Client/middleware.js
+++ Client/middleware.js
@@ -1,22 +1,33 @@
-// ## Clerk 'Proxy' ##
+// ## Clerk 'Middleware' ##
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
-const isPublicRoute = createRouteMatcher([
-    '/sign-in(.*)',
-    '/sign-up(.*)'
-])
+// # Define the clerk 'public' routes
+const isPublicRoute = createRouteMatcher(
+    [
+        '/sign-in(.*)',
+        '/sign-up(.*)'
+    ]
+)
 
-export default clerkMiddleware(async (auth, req) => {
-    if (!isPublicRoute(req)) {
-        await auth.protect()
+// # Define the clerk 'middleware'
+const clerkMiddleWare = clerkMiddleware(
+    async (auth, req) => {
+        if (!isPublicRoute(req)) {
+            await auth.protect()
+        }
     }
-})
+)
 
-export const config = {
+// # Define the clerk 'config'
+const clerkConfig = {
     matcher: [
-        // Skip Next.js internals and all static files, unless found in search params
+        // # Skip the 'Next.js' internals and all the 'static' files, unless found in the 'search' params
         '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-        // Always run for API routes
-        '/(api|trpc)(.*)',
-    ],
+        // # Always run for the 'API' routes
+        '/(api|trpc)(.*)'
+    ]
 }
+
+// # Export the clerk 'middleware' and the 'config'
+export default clerkMiddleWare
+export { clerkConfig }
