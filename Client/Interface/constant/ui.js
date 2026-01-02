fc706fe47f328871f684d3717fb5a0dc7a942f2b
Suraj-Encoding <dalvisuraj256@gmail.com>
2026-01-02 15:32:50 +0530

    logo patch

--- Client/Interface/constant/ui.js
+++ Client/Interface/constant/ui.js
@@ -1,8 +1,8 @@
 // # 'UI Data' Constant
 const UIData = {
     site_name: "Shortify",
-    black_logo: "black_logo.png", // # For "Light" Mode 
-    white_logo: "white_logo.png" // # For "Dark" Mode
+    black_logo: "./black_logo.png", // # For "Light" Mode 
+    white_logo: "./white_logo.png" // # For "Dark" Mode
 };
 
 export default UIData;
