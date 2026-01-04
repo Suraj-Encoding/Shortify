258c0d4e96c8b2726db9a92b84f42519adc38a18
Suraj-Encoding <dalvisuraj256@gmail.com>
2026-01-04 13:39:38 +0530

    updated the ui constant

--- Client/Interface/constant/ui.js
+++ Client/Interface/constant/ui.js
@@ -2,6 +2,7 @@
 const UIData = {
     site_title: "Shortify",
     site_description: "Modern URL Shortener",
+    site_logo: "logo.png",
     black_logo: "black_logo.png", // # For "Light" Mode 
     white_logo: "white_logo.png" // # For "Dark" Mode
 };
