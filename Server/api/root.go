2750cb506834d34719e7625bc34157590c448f56
Suraj-Encoding <dalvisuraj256@gmail.com>
2026-01-04 15:22:31 +0530

    welcome message change

--- Server/api/root.go
+++ Server/api/root.go
@@ -8,6 +8,6 @@ import (
 // # Root 'Page': ["/"]
 // # Note: Send the "welcome" message to the 'client'
 func RootPage(w http.ResponseWriter, r *http.Request) {
-	const welcomeMessage string = "🚀 Welcome To Shortify - Modern URL Shortener 🚀"
+	const welcomeMessage string = "🚀 Welcome to Shortify - Modern URL Shortener 🚀"
 	fmt.Fprintf(w, welcomeMessage)
 }
