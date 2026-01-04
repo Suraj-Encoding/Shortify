005c3116108d7ef9f91d908c338cfac7dd009868
Suraj-Encoding <dalvisuraj256@gmail.com>
2026-01-04 21:29:26 +0530

    redirect url patch

--- Server/api/redirect.go
+++ Server/api/redirect.go
@@ -25,35 +25,10 @@ func RedirectURL(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("🔗 Link URL Path:", linkURLPath)
 
 	linkURLPathParts := strings.Split(linkURLPath, "/")
-	linkURLPathPartLength := len(linkURLPathParts)
-
-	if linkURLPathPartLength != 2 {
-		errMsg = "🚫 Invalid link requested to shortify: Invalid link URL path found"
-		err = errors.New(errMsg)
-		utils.LogError(err, "API.RedirectURL")
-		errRes = schema.Error{
-			StatusCode: http.StatusNotFound,
-			Message:    errMsg,
-		}
-		utils.SetAppError(w, &errRes)
-		return
-	}
 
 	// # Get the 'username' and 'link slug' from the 'URL Path' of the requested 'link'
 	username := utils.GetTrimmedValue(linkURLPathParts[0])
 	linkSlug := utils.GetTrimmedValue(linkURLPathParts[1])
-
-	if username == "" || linkSlug == "" {
-		errMsg = "🚫 Invalid link requested to shortify: Empty username or link slug found"
-		err = errors.New(errMsg)
-		utils.LogError(err, "API.RedirectURL")
-		errRes = schema.Error{
-			StatusCode: http.StatusNotFound,
-			Message:    errMsg,
-		}
-		utils.SetAppError(w, &errRes)
-		return
-	}
 
 	// # Get the 'Destination URL' of the requested 'link'
 	destinationURL, err := app.GetDestinationURL(username, linkSlug)
