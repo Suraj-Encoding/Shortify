1c116e1ff613eaa5d7dba8087b1188203016f51a
Suraj-Encoding <dalvisuraj256@gmail.com>
2026-01-04 13:33:28 +0530

    updated the metadata and ui constant

--- Client/Interface/constant/metadata.js
+++ Client/Interface/constant/metadata.js
@@ -2,7 +2,7 @@
 const MetaData = {
   title: "Shortify - URL Shortener",
   description: "💲Shortify💲 ~ 🕸️ URL Shortener 🕸️",
-  icons: "logo.png",
+  icons: "logo.png"
 };
 
 export default MetaData;
--- Client/Interface/constant/ui.js
+++ Client/Interface/constant/ui.js
@@ -1,6 +1,7 @@
 // # 'UI Data' Constant
 const UIData = {
-    site_name: "Shortify",
+    site_title: "Shortify",
+    site_description: "Modern URL Shortener",
     black_logo: "black_logo.png", // # For "Light" Mode 
     white_logo: "white_logo.png" // # For "Dark" Mode
 };
