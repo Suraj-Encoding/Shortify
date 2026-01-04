79b65de56223ca9796e5f936b8f79786c70fdf9d
Suraj-Encoding <dalvisuraj256@gmail.com>
2026-01-04 20:47:49 +0530

    home page changed

--- Server/api/home.go
+++ Server/api/home.go
@@ -10,6 +10,8 @@ import (
 func ServeHomePage(w http.ResponseWriter, r *http.Request) {
 	const rootFolderName string = "public"
 	const homePageFileSubPath string = "html/index.html"
+
 	homePageFilePath := fmt.Sprintf("%s/%s", rootFolderName, homePageFileSubPath)
+
 	http.ServeFile(w, r, homePageFilePath)
 }
